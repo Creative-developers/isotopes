22802d40e1cd834a1896506b3369270d33c6ff6b
unknown <ahmednasrullah77@gmail.com>
2019-04-14 23:52:36 +0400

    adding index.php

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>buy isotope - stable isotopes, metal isotopes, gas isotopes, elemetal isotopes, radioactive isotopes.</title>
+    <meta charset=utf-8" />
+    <meta name="description" content="BuyIsotope - distribution of stable isotopes at competitive price: metal isotope, oxide isotope, carbonate isotope, gas isotopes, elemental isotopes." />
+    <meta name="keywords" content="stable isotope, NMR isotope, metal isotope, oxide isotope, carbonate isotope, gas isotopes, elemental isotopes, buy isotope, distribution of isotopes, isotope offer." />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Petr Vasiliev">
+    <meta name="robots" content="index,follow">
+    <meta name="revisit-after" content="2 days">
+<!-- Global site tag (gtag.js) - Google Ads: 1013962582 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-1013962582"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-1013962582');
+</script>
+<!-- Event snippet for Front webpage_Request a quote image -->
+<script>
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-1013962582/LwjhCOaSrooBENauv-MD',
+      'value': 1.0,
+      'currency': 'SEK',
+      'event_callback': callback
+  });
+  return false;
+}
+</script>
+<!-- Google Analytics UA-3875826-5 -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3875826-5']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics UA-40297494-1-->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40297494-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-40297494-1');
+</script>
+<!--[Bing - vasilievp account]-->
+  <meta name="msvalidate.01" content="93901A8DE40229E5C764B1FFF47C99DD" />
+  <meta name="msvalidate.01" content="7DF2BF446D6216E7C8F0F478BBD2B34E" />
+  <meta name="google-site-verification" content="j0T7QGGpsoNIKBG_2tcTKbfJnrI2UBiB8nW0ukn3yPk" />
+  <link rel="preload" href="css/style.css" as="style">
+  <link rel="preload" href="js/jquery-2.2.4.min.js" as="script">
+  <link rel="preload"  href="js/cufon-yui.js" as="script">
+  <link rel="preload"  href="js/cufon-replace.js" as="script">
+  <link rel="preload"  href="js/Lato_400.font.js" as="script">
+  <!--<script type="text/javascript" src="js/Lato_Light_300.font.js"></script>
+  <script type="text/javascript" src="js/Lato_700.font.js"></script>-->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- Header -->
+<header>
+  <div class="header-bg">
+        <div class="header-bg1">
+            <div class="main">
+              <div class="row">
+                    <h1><a href="request_quote_stable_isotope.php">Stable Isotope</a></h1>
+                  <nav> 
+                        <ul class="sf-menu">
+                        <li class="current"><a href="index.php" class="current">Home</a></li>
+                        <li><a href="stable_isotopes.php">Isotopes</a></li>
+                        <li><a href="request_quote_stable_isotope.php">Request a Quote</a></li>
+                        <li><a href="partners_stable_isotope.php">Partners</a></li>
+                        <li><a href="contact_stable_isotope.php">Contacts</a></li>
+                      </ul>
+                    <div class="clear"></div>
+                  </nav>
+                </div>
+            </div>
+            <div class="pad-slider">
+                    <div class="main-slider">
+                        <ul class="items">
+                            <li>
+                                <img src="images/slide1.jpg" alt="surprising isotope prices" />
+                                <div class="slider-banner">
+                                  <div class="slider-text">Discover</div>
+                                    <div class="slider-text1">surprising Isotope prices</div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide2.jpg" alt="isotopes from Stockholm" />
+                               <div class="slider-banner">
+                                  <div class="slider-text">Isotopes</div>
+                                    <div class="slider-text1">that you need</div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src="images/slide3.jpg" alt="clients choose isotope" />
+                                <div class="slider-banner">
+                                  <div class="slider-text">Clients</div>
+                                    <div class="slider-text1">choose us</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                  </div>
+        </div>    
+    </div>
+</header>
+<!-- Content -->
+<section id="content">
+<div class="content-bottom">
+  <div class="container_12">
+      <div class="wrapper">
+          <div class="grid_4">
+                <div class="box1-top">
+                    <div class="box1">
+                        <div class="padding">
+                            <div class="title-1">Common Isotopes</div>
+                            <span class="text-top"><h2><a href="iron-57_isotope.php">Iron-57 (57Fe, Fe-57)</a>,</h2>
+                            <h5><a href="calcium-44_isotope.php">Calcium-44 (44Ca, Ca-44)</a>,</h5>
+                            <h1><a href="zinc-68_isotope.php">Zinc-68 (68Zn, Zn-68)</a>,</h1>
+                            <h3><a href="silicon-29_isotope.php">Silicon-29 (29Si, Si-29)</a>,</h3>
+                             <h4><a href="cupper-65_isotope.php">Cupper-65 (65Cu, Cu-65)</a>,</h4>
+                            <h1><a href="calcium-42_isotope.php">Calcium-42 (42Ca, Ca-42)</a>,</h1>
+                             <h4><a href="potassium-40_isotope.php">Potassium-40 (40K, K-40)</a>,</h4>
+<h3><a href="magnesium-25_isotope.php">Magnesium-25 (25Mg, Mg-25)</a></h3></span>
+                        </div>
+                    </div>
+                </div><br>
+          <center><a href="request_quote_stable_isotope.php"><IMG SRC="request_a_quote_w.jpg" ALT="Request a quote for isotope"  onclick="return gtag_report_conversion('https://www.buyisotope.com/request_quote_stable_isotope.php')"></a></center><br><br>
+<center><a href="https://www.bigsciencesweden.se/in-english/"><IMG SRC="big_science_sweden.png" ALT="Big Science Sweden."></center><br><br>
+<center><a href="http://www.tryggverksamhet.nu/certifierad/neonest" target="_blank" style="text-decoration: none;"><img style="border:0px;" oncontextmenu="return false" title="Neonest AB company is credit worthy according to Soliditet and UC." alt="Neonest AB company company is credit worthy according to Soliditet and UC." src="images/buyisotope_neonest_guldsigill_2.png" /></a>
+</center>
+          <center><a href="https://www.soliditet.se/lang/en_GB/RatingGuideline" target="_blank" style="text-decoration: none;"><img style="border:0px;" oncontextmenu="return false" title="Our company is credit worthy according to Bisnode's credit assessment system that is based on a number of decision rules. This credit rating is updated on a daily basis&#44; and always shows the current rating and date." alt="Our company is credit worthy according to Bisnode's credit assessment system that is based on a number of decision rules. This credit rating is updated on a daily basis&#44; and always shows the current rating and date." id="img_273_87_px_tr1" src="https://merit.soliditet.se/merit/imageGenerator/display?lang=EN&country=SE&cId=KBsJfup0d11kXxi24jEYmw%3D%3D&cUid=4HoDeRZy%2BW4%3D&imgType=img_273_87_px_tr1" /></a>
+          </center>
+            </div>
+            <div class="grid_8">
+               <div class="title-1 title-1-top">Who are we?</div>
+                 <div class="wrapper pad-top">
+                  Buyisotope is integral part of Neonest AB company. 
+                  The company combines a team of professionals from Chemistry and Physics to provide high quality products, support of the products,  project development and innovations for our customers. <br><br>
+                  BuyIsotope team particularly focuses on distribution and sales of stable isotopes. <br>BuyIsotope provides high quality stable isotopes worldwide at competitive price.<br>
+           <br>             
+                    <div class="title-1">Below is list of stable isotopes, which you can order from BuyIsotope:</div>
+            <p align="justify">
+           <div class="text5">Isotopes enriched using electromagnetic separation:</div>  
+          <ul class="list-1"> 
+<li><a href="barium_isotopes_130_132_134_135_136_137_138.php">Barium stable isotopes: Ba-130, Ba-132, Ba-134, Ba-135, Ba-136, Ba-137, Ba-138;</a></li>
+<li><a href="cadmium_isotopes_106_108_110_111_112_113_114_116.php">Cadmium stable isotopes: Cd-106, Cd-108, Cd-110, Cd-111, Cd-112, Cd-113, Cd-114, Cd-116;</a></li>
+<li><a href="calcium_isotopes_40_42_43_44_46_48.php">Calcium stable isotopes: Ca-40, Ca-42, Ca-43, Ca-44, Ca-46, Ca-48;</a></li>
+<li><a href="cerium_isotopes_136_138_140_142.php">Cerium stable isotopes: Ce-136, Ce-138, Ce-140, Ce-142;</a></li>
+<li><a href="chromium_isotopes_50_52_53_54.php">Chromium stable isotopes: Cr-50, Cr-52, Cr-53, Cr-54;</a></li>
+<li><a href="copper_isotopes_63_65.php">Copper stable isotopes: Cu-63, Cu-65;</a></li>
+<li><a href="dysprosium_isotopes_156_158_160_161_162_163_164.php">Dysprosium stable isotope: Dy-156, Dy-158, Dy-160, Dy-161, Dy-162, Dy-163, Dy-164;</a></li>
+<li><a href="erbium_isotopes_162_164_166_167_168_170.php">Erbium stable isotope: Er-162, Er-164, Er-166, Er-167, Er-168, Er-170;</a></li>
+<li><a href="europium_isotopes_151_153.php">Europium stable isotopes: Eu-151, Eu-153;</a></li>
+<li><a href="gadolinium_isotopes_152_154_155_156_157_158_160.php">Gadolinium stable isotopes: Gd-152, Gd-154, Gd-155, Gd-156, Gd-157, Gd-158, Gd-160;</a></li>
+<li><a href="gallium_isotopes_69_71.php">Gallium stable isotopes: Ga-69, Ga-71;</a></li>
+<li><a href="hafnium_isotopes_174_176_177_178_180.php">Hafnium stable isotopes: Hf-174, Hf-176, Hf-177, Hf-178, Hf-179, Hf-180;</a></li>
+<li><a href="indium_isotopes_113_115.php">Indium stable isotope: In-113;</a></li>
+<li><a href="lanthanum_isotopes_138_139.php">Lanthanum stable isotopes: La-138, La-139;</a></li>
+<li><a href="lead_isotopes_204_206_207_208.php">Lead stable isotopes: Pb-204, Pb-206, Pb-207, Pb-208;</a></li>
+<li><a href="lutetium_isotopes_175_176.php">Lutetium stable isotopes: Lu-175, Lu-176;</a></li>
+<li><a href="magnesium_isotopes_24_25_26.php">Magnesium stable isotopes: Mg-24, Mg-25, Mg-26;</a></li>
+<li><a href="neodymium_isotopes_142_143_144_146_148_150.php">Neodymium stable isotopes: Nd-142, Nd-143, Nd-144, Nd-145, Nd-146, Nd-148, Nd-150;</a></li>
+<li><a href="potassium_isotopes_39_40_41.php">Potassium stable isotopes: K-39, K-40, K-41;</a></li>
+<li><a href="rhenium_isotopes_185_187.php">Rhenium stable isotopes: Re-185, Re-187;</a></li>
+<li><a href="rubidium_isotopes_85_87.php">Rubidium stable isotopes: Rb-85, Rb-87;</a></li>
+<li><a href="samarium_isotopes_144_147_148_149_150_152_154.php">Samarium stable isotopes: Sm-144, Sm-147, Sm-148, Sm-149, Sm-150, Sm-152, Sm-154;</a></li>
+<li><a href="silver_isotopes_107_109.php">Silver stable isotopes: Ag-107, Ag-109;</a></li>
+<li><a href="strontium_isotopes_84_86_87_88.php">Strontium stable isotopes: Sr-84, Sr-86, Sr-87, Sr-88;</a></li>
+<li><a href="thallium_isotopes_203_205.php">Thallium stable isotopes: Tl-203, Tl-205;</a></li>
+<li><a href="titanium_isotopes_46_47_48_49_50.php">Titanium stable isotopes: Ti-46, Ti-47, Ti-48, Ti-49, Ti-50;</a></li>
+<li><a href="ytterbium_isotopes_168_170_171_172_173_174_176.php">Ytterbium stable isotopes: Yb-168, Yb-170, Yb-171, Yb-172, Yb-173, Yb-174, Yb-176;</a></li>
+<li><a href="zinc_isotopes_64_66_67_68_70.php">Zinc stable isotopes: Zn-64, Zn-66, Zn-67, Zn-68, Zn-70;</a></li>
+<li><a href="zirconium_isotopes_90_91_92_94_96.php">Zirconium stable isotopes: Zr-90, Zr-91, Zr-92, Zr-94, Zr-96;</a></li>
+    </ul>
+           </p> 
+           <p align="justify">
+         <div class="text5">Isotopes enriched using gas centrifugal separation:</div>
+         <ul class="list-1"> 
+<li><a href="argon_isotopes_36_38_40.php">Argon stable isotopes: Ar-36, Ar-38, Ar-40;</a></li>
+<li><a href="tungsten_isotopes_180_182_183_184_186.php">Tungsten stable isotopes: W-180, W-182, W-183, W-184, W-186;</a></li>
+<li><a href="germanium_isotopes_70_72_73_74_76.php">Germanium stable isotopes: Ge-70, Ge-72, Ge-73, Ge-74, Ge-76;</a></li>
+<li><a href="iron_isotopes_54_56_57_58.php">Iron stable isotopes: Fe-54, Fe-56, Fe-57, Fe-58;</a></li>
+<li><a href="iridium_isotopes_191_193.php">Iridium stable isotopes: Ir-191, Ir-193;</a></li>
+<li><a href="cadmium_isotopes_106_108_110_111_112_113_114_116.php">Cadmium stable isotopes: Cd-106, Cd-108, Cd-110, Cd-111, Cd-112, Cd-113, Cd-114, Cd-116;</a></li>
+<li><a href="silicon_isotopes_28_29_30.php">Silicon stable isotopes: Si-29, Si-29, Si-28, Si-30;</a></li>
+<li><a href="krypton_isotopes_78_80_82_83_84_86.php">Krypton stable isotopes: Kr-78, Kr-80, Kr-82, Kr-83, Kr-84, Kr-86;</a></li>
+<li><a href="xenon_isotopes_124_126_128_129_130_131_132_134_136.php">Xenon stable isotopes: Xe-124, Xe-126, Xe-128, Xe-129, Xe-130, Xe-131, Xe-132, Xe-134, Xe-136;</a></li>
+<li><a href="molybdenum_isotopes_92_94_95_96_97_98_100.php">Molybdenum stable isotopes: Mo-92, Mo-94, Mo-95, Mo-97, Mo-98, Mo-100;</a></li>
+<li><a href="nickel_isotopes_58_60_61_62_64.php">Nickel stable isotopes: Ni-58, Ni-60, Ni-61, Ni-62, Ni-64;</a></li>
+<li><a href="tin_isotopes_112_116_124.php">Tin stable isotopes: Sn-112, Sn-116, Sn-124;</a></li>
+<li><a href="osmium_isotopes_184_186_187_188_189_190_192.php">Osmium stable isotopes: Os-184, Os-186, Os-187, Os-188, Os-189, Os-190, Os-192;</a></li>
+<li><a href="palladium_isotopes_102_104_105_106_108_110.php">Palladium stable isotopes: Pd-102, Pd-104, Pd-105, Pd-106, Pd-108, Pd-110;</a></li>
+<li><a href="selenium_isotopes_74_76_77_78_80_82.php">Selenium stable isotopes: Se-76, Se-77, Se-78, Se-80, Se-82;</a></li>
+<li><a href="sulfur_isotopes_32_33_34_36.php">Sulfur stable isotopes: S-32, S-33, S-34, S-36;</a></li>
+<li><a href="tellurium_isotopes_120_122_123_124_125_126_128_130.php">Tellurium stable isotopes: Te-120, Te-122, Te-123, Te-124, Te-125, Te-126, Te-128, Te-130;</a></li>
+<li><a href="zinc_isotopes_64_66_67_68_70.php">Zinc stable isotopes: Zn-64, Zn-66, Zn-67, Zn-68, Zn-70;</a></li>
+      </ul>
+            </p>
+                 </div>
+                <!-- <div class="border1"></div>
+                 <div class="title-1">Latest Projects and Publications about stable isotopes</div>
+                    <div class="wrapper img-top">
+                    <div class="grid_4 alpha">
+                      <img src="images/page1-img2.jpg" alt="publications on stable isotopes">
+                        <a href="publications.php" class="link2">Publications stable isotopes</a>
+                      </div>
+                    </div>-->
+                 </div>
+            </div>
+        </div>
+    </div>
+   </div> 
+</section>
+<!-- Footer -->
+<footer>
+<?php include ($_SERVER['DOCUMENT_ROOT']."/footer.php"); ?>
+<!-- <?php include('./footer.php');?> -->
+</footer> 
+<!--- js files --->
+<!-- <script src="js/jquery-1.7.1.min.js"></script> -->
+<script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="js/tms-0.4.x.js"></script>
+<script type="text/javascript" src="js/uCarausel.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+<script type="text/javascript">
+  jQuery.browser = {};
+  (function () {
+      jQuery.browser.msie = false;
+      jQuery.browser.version = 0;
+      if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+          jQuery.browser.msie = true;
+          jQuery.browser.version = RegExp.$1;
+      }
+  })();
+</script>
+<script type="text/javascript">
+  $(function(){
+    $(".main-slider")._TMS({
+        show: 0,
+        pauseOnHover: false,
+        prevBu: false,
+        nextBu: false,
+        playBu: false,
+        duration: 1000,
+        preset: "simpleFade",
+        pagination: true,
+        pagNums: false,
+        slideshow: 7000,
+        numStatus: false,
+        bannerCl: "slider-banner",
+        banners: "fade",
+        waitBannerAnimation: false,
+        progressBar: false
+    });
+  });
+</script>
+<!-- <script src="js/script.js"></script> -->
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Lato_400.font.js"></script>
+</body>
+</html>
